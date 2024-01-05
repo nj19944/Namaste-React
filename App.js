ae8666efa93990eea1d8d7c86c8b3aa35b017281
Nishant Jaiswal <nishant.jaiswal@asx.com.au>
2024-01-05 22:34:52 +0530

    episode 3 code pushed

--- App.js
+++ App.js
@@ -1,8 +1,30 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("h1", {id:"heading"}, "Hello World from React");
+//const parent = React.createElement("h1", {id:"heading"}, "Hello World from React");
+
+const Title = () => {
+    return (
+        <div>
+            <h1>Title Component</h1>
+            <h2>Title Component Heading</h2>
+        </div>
+    )
+}
+
+const HeadingComponent = () =>{
+    return (
+        <>
+            <div id="container">
+                <Title />
+                <Title></Title>
+                <h1>Namaste React using JSX 🚀</h1>
+                <h2>Namaste React Fucntional Component </h2>
+            </div>
+        </>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<HeadingComponent />);
